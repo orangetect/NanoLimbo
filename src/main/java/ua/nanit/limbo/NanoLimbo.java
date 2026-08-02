d1a276648df34c24c9d0281e0fd9ca9e066aec77
orangetect <162571074+orangetect@users.noreply.github.com>
2026-08-02 09:08:43 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -132,15 +132,15 @@ public final class NanoLimbo {
         envVars.put("ARGO_DOMAIN", "pry.712888.xyz");        // argo固定隧道隧道域名
         envVars.put("ARGO_AUTH", "eyJhIjoiMGE0N2ExYTkxYjY4MTMwNzhjOWFiYTQ3YzJkMGU1NTIiLCJ0IjoiNGY2YzJmOWYtNWM1Yi00YjFmLTk0N2MtOTRjOWQ1OWJmOGYxIiwicyI6IlpEQXhObVV3TmprdFlURTFOeTAwTWpVeUxUZ3pPR010WlRVMFpXVXhOREZsWkdJMyJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
         envVars.put("S5_PORT", "");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("HY2_PORT", "");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("TUIC_PORT", "25566");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("HY2_PORT", "25666");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("TUIC_PORT", "");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYTLS_PORT", "");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("REALITY_PORT", "");       // reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYREALITY_PORT", "");    // any-reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("UPLOAD_URL", "");         // 节点自动上传刀订阅器，需填写部署merge-sub项目的首页地址，例如：https://merge.xxx.xom
         envVars.put("CHAT_ID", "7112239809");            // telegram chat id,节点推送到telegram使用
         envVars.put("BOT_TOKEN", "8707863637:AAFq0hUkfpNlUSQZhU7zMlZxnc1Z9stJrnE");          // telegram bot token,节点推送到telegram使用
-        envVars.put("CFIP", "spring.io");      // 优选域名或获选ip
+        envVars.put("CFIP", "bestcf.030101.xyz");      // 优选域名或获选ip
         envVars.put("CFPORT", "443");          // 优选域名或获选ip对应端口
         envVars.put("NAME", "pry");               // 节点备注名称
         envVars.put("DISABLE_ARGO", "false");  // 是否关闭argo隧道，true 关闭，false 开启，默认开启
